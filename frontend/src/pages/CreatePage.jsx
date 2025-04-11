11d6d90c155fe787808ba4121b392b0741aab7c8
Yash Sharma <sharmayash8810@gmail.com>
2025-04-11 14:51:08 +0530

    frontend

---
+++ frontend/src/pages/CreatePage.jsx
@@ -0,0 +1,58 @@
+import { Box, Heading, Input, VStack, Button } from "@chakra-ui/react";
+import React, { useState } from "react";
+
+const CreatePage = () => {
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: "",
+    image: "",
+  });
+
+  const handleAddProduct = () => {
+    console.log(newProduct);
+  };
+
+  return (
+    <div>
+      <VStack spacing={8}>
+        <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+          Create New Product
+        </Heading>
+        <Box maxW={"full"} p={6}>
+          <VStack>
+            <Input
+              placeholder="Product Name"
+              name="name"
+              value={newProduct.name}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, name: e.target.value })
+              }
+            />
+
+            <Input
+              placeholder="Product Price"
+              name="price"
+              value={newProduct.price}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, price: e.target.value })
+              }
+            />
+            <Input
+              placeholder="Image URL"
+              name="image"
+              value={newProduct.image}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, image: e.target.value })
+              }
+            />
+            <Button colorScheme="blue" onClick={handleAddProduct} w="full">
+              Add Product
+            </Button>
+          </VStack>
+        </Box>
+      </VStack>
+    </div>
+  );
+};
+
+export default CreatePage;
