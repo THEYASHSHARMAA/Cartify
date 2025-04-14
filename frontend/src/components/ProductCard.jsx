917c5b39860f63f20a2db871ca9745eb717b2863
Yash Sharma <sharmayash8810@gmail.com>
2025-04-14 12:43:34 +0530

    home page

---
+++ frontend/src/components/ProductCard.jsx
@@ -0,0 +1,89 @@
+//import { Box, Heading, Image, Text } from "@chakra-ui/react";
+import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Button,
+  Heading,
+  HStack,
+  IconButton,
+  Image,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useColorModeValue,
+  useDisclosure,
+  useToast,
+  VStack,
+} from "@chakra-ui/react";
+import { useProductStore } from "../store/product";
+
+const ProductCard = ({ product }) => {
+  const textColor = useColorModeValue("gray.600", "gray.200");
+  const bg = useColorModeValue("white", "gray.800");
+
+  const toast = useToast();
+  const { deleteProduct } = useProductStore();
+  const handleDeleteProduct = async (pid) => {
+    const { success, message } = await deleteProduct(pid);
+    if (!success) {
+      toast({
+        title: "Error",
+        description: message,
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+    } else {
+      toast({
+        title: "Success",
+        description: message,
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  };
+
+  return (
+    <Box
+      shadow={"lg"}
+      rounded={"lg"}
+      overflow={"hidden"}
+      transition={"all 0.3s"}
+      bg={bg}
+      _hover={{ transform: "translateY(-5px)", shadow: "xl" }}
+    >
+      <Image
+        src={product.iamge}
+        alt={product.name}
+        h={48}
+        w="full"
+        objectFit="cover"
+      />
+      <Box p={4}>
+        <Heading as="h3" size={"md"} mb={2}>
+          {product.name}
+        </Heading>
+        <Text fontWeight={"bold"} fontSize={"xl"} color={textColor} mb={4}>
+          ${product.price}
+        </Text>
+
+        <HStack spacing={3}>
+          <IconButton icon={<EditIcon />} color={"blue"} />
+          <IconButton
+            icon={<DeleteIcon />}
+            onClick={() => handleDeleteProduct(product._id)}
+            colorScheme="red"
+          />
+        </HStack>
+      </Box>
+    </Box>
+  );
+};
+export default ProductCard;
--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -19,6 +19,22 @@ export const useProductStore = create((set) => ({
     //here we add newproduct globally
     return { success: true, message: "Product Created succesfully" };
   },
+  fetchProducts: async () => {
+    const res = await fetch("/api/products");
+    const data = await res.json();
+    set({ products: data.data });
+  },
+  deleteProduct: async (pid) => {
+    const res = await fetch(`./api/products/${pid}`, {
+      method: "Delete",
+    });
+    const data = await res.json();
+    if (!data.success) return { success: false, message: data.message };
+    set((state) => ({
+      products: state.products.filter((product) => product._id !== pid),
+    }));
+    return { success: true, message: data.message };
+  },
 }));
 // its a global state variable it like nrml usestate in
 // which we give product and setProduct  as localy
